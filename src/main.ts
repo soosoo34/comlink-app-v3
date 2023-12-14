b3edd20fb1b8759b9a64f417db14efdc5e6da133
Ramos Bryan <ramosbryanlunel@gmail.com>
2023-12-14 09:59:29 +0100

    feat: add ngxs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
+import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsModule } from '@ngxs/store';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      NgxsModule.forRoot([], {
+        developmentMode: !environment.production,
+        selectorOptions: {
+          suppressErrors: false,
+          injectContainerState: false,
+        },
+      }),
+      NgxsLoggerPluginModule.forRoot({
+        collapsed: false,
+        disabled: environment.production,
+      }),
+      NgxsReduxDevtoolsPluginModule.forRoot()
+    ),
+  ],
+}).catch(err => console.error(err));
