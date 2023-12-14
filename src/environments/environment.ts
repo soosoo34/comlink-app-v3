7d5ee466d032dcc769f086e8c912d8e2a9f23ce6
Ramos Bryan <ramosbryanlunel@gmail.com>
2023-12-14 09:24:54 +0100

    feat: add env file

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  /*apiUrl: 'http://localhost:8080',*/
+  apiUrl: 'https://api-v2.comlink-app.com',
+  /*apiUrl: 'https://api-dev.comlink-app.com',*/
+};
